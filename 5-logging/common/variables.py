a406281f0619befcd510ff7510c56f09bce9c424
GoodGuy4221 <75629090+GoodGuy4221@users.noreply.github.com>
2022-05-27 15:43:12 +0400

    5-logging

---
+++ 5-logging/common/variables.py
@@ -0,0 +1,28 @@
+import logging
+
+MIN_PORT_NUMBER = 1024
+MAX_PORT_NUMBER = 65535
+
+ANON_ACCOUNT_NAME = 'anonymous'
+
+DEFAULT_LISTEN_ADDRESS = ''
+DEFAULT_PORT = 7777
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 1
+MAX_PACKAGE_LENGTH = 1024
+ENCODING = 'utf-8'
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+RESPONDEFAULT_IP_ADDRESSSE = 'respondefault_ip_addressse'
+
+STATUS_OK = 200
+STATUS_BAD_REQUEST = 400
+
+LOGGING_LEVEL = logging.DEBUG
