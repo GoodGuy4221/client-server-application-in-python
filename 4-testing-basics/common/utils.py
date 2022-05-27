8b726d49cf25036a61bf25e3e703a13be5d4abf1
GoodGuy4221 <75629090+GoodGuy4221@users.noreply.github.com>
2022-05-27 11:41:41 +0400

    4-testing-basics

---
+++ 4-testing-basics/common/utils.py
@@ -0,0 +1,24 @@
+import json
+from socket import socket
+
+from .variables import *
+from errors import NotDict, NotBytes
+
+
+class Utils:
+    def get_message(self, client: socket) -> dict:
+        encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+        if isinstance(encoded_response, bytes):
+            json_response = encoded_response.decode(ENCODING)
+            response = json.loads(json_response)
+            if isinstance(response, dict):
+                return response
+            raise NotDict
+        raise NotBytes
+
+    def send_message(self, sock, message: dict):
+        if not isinstance(message, dict):
+            raise NotDict
+        json_message: str = json.dumps(message)
+        encoded_message: bytes = json_message.encode(encoding=ENCODING)
+        sock.send(encoded_message)
